089018dc0005db002c5798b93cf26bf649eabd33
mehedi hasan <w2b.programmer@gmail.com>
2023-08-29 18:59:48 +0600

    add searching

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone=async ()=>{
-    const res=await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone=async (searchText)=>{
+    const res=await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data=await res.json();
     phones=data.data;
     console.log(data)
@@ -10,15 +10,24 @@ const loadPhone=async ()=>{
 const displayPhones=phones=>{
     // console.log(phones);
     const phoneContainer=document.getElementById('phone-container');
+    phoneContainer.textContent='';
+    const showAll=document.getElementById('show-all');
+    if(phones.length > 9){
+        showAll.classList.remove('hidden');
+    }
+    else{
+        showAll.classList.add('hidden');
+    }
+    phones=phones.slice(0,9)
     phones.forEach(phone => {
         console.log(phone);
 
         const phoneCard=document.createElement('div');
-        phoneCard.classList=`card w-96 bg-gray-100 shadow-xl`;
+        phoneCard.classList=`card p-4 bg-gray-100 shadow-xl `;
         phoneCard.innerHTML=`
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
         <div class="card-body">
-          <h2 class="card-title">Shoes!</h2>
+          <h2 class="card-title">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div class="card-actions justify-end">
             <button class="btn btn-primary">Buy Now</button>
@@ -32,7 +41,12 @@ const displayPhones=phones=>{
 }
 
 
-
+const handleSearch=()=>{
+    const searchField=document.getElementById('search-field');
+    const searchText=searchField.value;
+    console.log(searchText)
+    loadPhone(searchText)
+}
 
 
 loadPhone();
